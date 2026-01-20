b0f1073014bf4a792e3b955e562b09f036af0616
hamzaghafran <hamzaghafran56@gmail.com>
2026-01-20 02:34:40 -0800

    added retrieval pipeline

--- src/retrieval.py
+++ src/retrieval.py
@@ -3,15 +3,35 @@ from sentence_transformers import SentenceTransformer
 from opensearchpy import OpenSearch
 import os
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
 
 
 load_dotenv()
 HOST = os.getenv("OPENSEARCH_HOST")
-PORT = int(os.getenv("OPENSEARCH_PORT"))
+PORT = os.getenv("OPENSEARCH_PORT")
 INDEX_NAME = os.getenv("INDEX_NAME")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(
+    model = "llama-3.1-8b-instant",
+    temperature = 0
+)
 
 client = OpenSearch(hosts=[{"host": HOST, "port": PORT}])
 model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+prompt = ChatPromptTemplate.from_template("""
+You are an AI assistant.
+Use ONLY the context below to answer the question.
+
+Context:
+{context}
+
+Question:
+{question}
+""")
 
 def retrieve(query, k=3):
     vector = model.encode(query).tolist()
@@ -23,7 +43,21 @@ def retrieve(query, k=3):
     return [hit["_source"]["metadata"]["text"] for hit in res["hits"]["hits"]]
 
 
+def rag_answer(question: str):
+    docs = retrieve(question)
 
-results = retrieve("")
-for r in results:
-    print(r)
+    context = "\n\n".join(docs)
+
+    messages = prompt.format_messages(
+        context=context,
+        question=question
+    )
+
+    response = llm.invoke(messages)
+    return response.content
+
+
+
+if __name__ == "__main__":
+    answer = rag_answer("What is Hamza's experience in machine learning?")
+    print(answer)
