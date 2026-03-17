b2192db8ae37ad36412c5a79d9762096f9cb6b77
hamzaghafran <hamzaghafran56@gmail.com>
2026-03-16 18:13:47 -0700

    developed upload file route for injection

--- src/api.py
+++ src/api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 from injection import injest_pdf
 from retrieval import rag_answer
 
@@ -8,6 +8,26 @@ router = APIRouter(
     tags = "User"
 )
 
-@router.post("/ingest")
-def ingest():
+@router.post("/upload_file")
+@app.post("/upload-pdf")
+async def upload_pdf(file: UploadFile = File(...)):
+    
+    if not file.filename.endswith(".pdf"):
+        return {"error": "Only PDF files are allowed"}
+    
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    upload_file(file_path)
+
+    return {
+        "message": "File uploaded and processed successfully",
+        "filename": file.filename
+    }
+    
+
+@router.post("/retrive")
+def retriver(query:str):
     pass
